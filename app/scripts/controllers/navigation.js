83c267bba0c4731638ff9d6a3bb6b958c55575f6
yaniv ran <yaniv.ran@gmail.com>
2015-01-27 11:21:18 -0800

    added tests to test the navigation controller

--- app/scripts/controllers/navigation.js
+++ app/scripts/controllers/navigation.js
@@ -2,6 +2,7 @@
 angular.module('swFrontApp')
   .controller('NavigationCtrl', function ($scope, $location, $resource) {
   	$scope.isActive = function(path){
-  		return path === $location.path();
+  		var currentpath = $location.path().split('/')[1].split('?')[0];
+  		return currentpath === path.split('/')[1]; 
   	};
   });
